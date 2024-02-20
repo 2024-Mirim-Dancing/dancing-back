2b879ae95fc66017086635218ab29db23d5d79e3
mic050r <103114387+mic050r@users.noreply.github.com>
2024-02-21 00:40:40 +0900

    🗃️ [feat] : mongoDB 연결 확인 파일 추가

---
+++ test.py
@@ -0,0 +1,18 @@
+import pymongo
+
+# MongoDB 연결 정보
+mongo_host = 'localhost'  # 또는 컨테이너의 IP 주소
+mongo_port = 27017
+mongo_user = 'admin'
+mongo_password = '1234'
+
+# MongoDB에 연결
+try:
+    client = pymongo.MongoClient(f'mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}')
+    db = client.admin  # 연결 테스트를 위해 admin 데이터베이스 사용
+    print("Connected to MongoDB successfully!")
+except pymongo.errors.ConnectionFailure as e:
+    print(f"Failed to connect to MongoDB: {e}")
+finally:
+    if 'client' in locals():
+        client.close()
