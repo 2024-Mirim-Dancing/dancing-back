30f391688a9e60f650c1787463d6eef1304e3d61
mic050r <s2216@e-mirim.hs.kr>
2024-05-04 05:34:49 +0900

    🔨  [feat] (#10) : Swagger UI를 사용하여 API 문서 자동화

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path
 from fastapi.responses import JSONResponse
 from pymongo import MongoClient
 import os
@@ -13,9 +13,17 @@ user_collection = db["user"]
 
 @app.get("/")
 async def index():
-    return "바보"
-@app.post("/score/{user_id}/{score}")
-async def submit_score(user_id: str, score: int):
+    return "home"
+
+@app.post("/score/{user_id}/{score}", tags=["score"])
+async def submit_score(user_id: str = Path(..., title="사용자 ID", description="사용자의 고유한 ID"),
+                       score: int = Path(..., title="점수", description="점수")):
+    """
+    사용자의 점수를 제출합니다.
+
+    - **user_id**: 사용자의 고유한 ID입니다.
+    - **score**: 제출할 점수입니다.
+    """
     # 사용자 아이디로 검색 후 결과가 없으면 새로운 사용자 추가
     existing_user = user_collection.find_one({"user_id": user_id})
     if not existing_user:
@@ -27,7 +35,10 @@ async def submit_score(user_id: str, score: int):
 
     return JSONResponse(content={"message": "Score submitted successfully"})
 
-@app.get("/ranking")
+@app.get("/ranking", tags=["ranking"])
 async def get_ranking():
+    """
+    사용자의 랭킹을 가져옵니다.
+    """
     ranking = list(user_collection.find(projection={"_id": 0}).sort("score", -1))
     return JSONResponse(content={"ranking": ranking})
